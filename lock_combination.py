d1b4beba6333eb8806b68bf949375e0c1086bec7
manasbobade2023 <72960975+manasbobade2023@users.noreply.github.com>
2022-04-26 10:57:25 +0530

    Update lock_combination.py

--- lock_combination.py
+++ lock_combination.py
@@ -1,8 +1,6 @@
 list_of_numbers=[0,1,2,3,4,5,6,7,8,9]
 original=list(map(int,input("enter the original state of the lock").split(",")))
-print(original)
 to_get=list(map(int,input("enter the desired state of the lock").split(",")))
-print(to_get)
 min_steps=[]
 forward=[]
 backward=[]
@@ -17,7 +15,6 @@ for i in range(0,len(original)):
             break
         a=(a+1)%10
         count+=1
-print(forward)     
 for i in range(0,len(original)):
     a=original[i]
     b=to_get[i]
@@ -29,10 +26,16 @@ for i in range(0,len(original)):
             break
         a=(a-1)%10
         count+=1
-print(backward)
 for i in range(0,len(original)):
-    a=forward[i]
-    b=backward[i]
-    print(min(a,b))
-    #min_steps.append(min(a,b))
-print(min_steps)    
+    m=forward[i]
+    n=backward[i]
+    if m<n:
+        min_steps.append(m)
+    else:
+        min_steps.append(n)
+print("min steps for each combinations are=",min_steps)
+total_sum=0
+for i in min_steps:
+    total_sum+=i
+print("total minimun steps=",total_sum)    
+    
